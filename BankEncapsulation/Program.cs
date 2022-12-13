11e1996808ec57743a199413ed40c589f2322313
Jessica Gonzales <jgonzales0688@gmail.com>
2022-12-13 15:16:47 -0600

    Exercise 1

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+    static void Main(string[] args)
+    {
+        var userAccount = new BankAccount();
+        
+        Console.WriteLine("How much will you be depositing today?");
+        var depositAmount =double.Parse(Console.ReadLine());
+            
+        userAccount.Deposit(depositAmount);
+
+        Console.WriteLine($"Your new balance is: {userAccount.GetBalance()}");
+    }
+    
+    }
+
+}
+
+
+
